1a60bc95cce15729b9585c84549cdb0123968bc6
Archy de Berker <archy@elementai.com>
2018-05-01 11:05:57 +0100

    Reorganize assets and include in package

---
+++ pianoplayer/constants.py
@@ -0,0 +1,5 @@
+import os
+parentdir = os.path.dirname(os.path.realpath(__file__))
+
+PIANO = os.path.join(parentdir, 'assets/piano.jpg')
+KEYMAP = os.path.join(parentdir, 'assets/key_map.pkl')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pianoplayer',
+    version='0.0.1',
+    packages=find_packages(exclude=('tests', 'docs')),
+    url='',
+    license='',
+    author='archydeberker',
+    author_email='archy.deberker@gmail.com',
+    description='',
+    include_package_data=True,
+)
